ca5e13436687ba15d339847c40a81705c5fc4ff9
Kervcodes <kervintz84@hotmail.com>
2026-06-22 18:20:50 -0400

    wire Footer into App and comment out Testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import {Hero} from "@/sections/Hero";
 import {About} from "@/sections/About";
 import {Projects} from "@/sections/Projects";
 import {Experience} from "@/sections/Experience";
-import {Testimonials} from "@/sections/Testimonials";
+// import {Testimonials} from "@/sections/Testimonials";
 import {Contact} from "@/sections/Contact";
+import {Footer} from "@/layout/Footer";
 
 function App() {
 
@@ -16,9 +17,10 @@ function App() {
         <About />
         <Projects />
         <Experience />
-        <Testimonials />
+        {/* <Testimonials /> */}
         <Contact />
       </main>
+      <Footer />
     </div>
   )
 }
